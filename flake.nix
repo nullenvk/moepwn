2131eda2b5bf69b4a4fea016788d3f6e8ad5f516
nullenvk <nullenvk@protonmail.com>
2025-09-26 01:57:11 +0200

    Add a simple flake env for OCaml development

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "OCaml Template";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        nativeBuildInputs = with pkgs; [
+          ocaml
+          ocamlformat
+        ]
+        ++ (with pkgs.ocamlPackages; [
+          dune_3
+          utop
+          ocaml-lsp
+          odoc
+        ]);
+
+        buildInputs = with pkgs; [];
+      in {
+        devShells.default = pkgs.mkShell {inherit nativeBuildInputs buildInputs;};
+      }
+    );
+}
