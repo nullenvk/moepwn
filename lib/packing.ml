a3e1a9a7d1dd40aa51231cf171ee3251c6452e55
nullenvk <nullenvk@protonmail.com>
2025-10-03 01:51:38 +0200

    Finish packing implementation

--- lib/packing.ml
+++ lib/packing.ml
@@ -1,5 +1,8 @@
 module type Endian = sig
     val p64 : int64 -> string 
+    val p32 : int32 -> string
+    val p16 : int -> string 
+    val p8 : int -> string 
 end
 
 let rec pack_nbytes (x : int64) (n : int) (acc : char list) : char list=
@@ -15,16 +18,16 @@ module LE : Endian = struct
     let packn n x = pack_nbytes x n [] |> list_to_str
 
     let p64 = packn 8
-    let p32 = packn 4
-    let p16 = packn 2
-    let p8 = packn 1
+    let p32 x = Int64.of_int32 x |> packn 4
+    let p16 x = Int64.of_int x |> packn 2
+    let p8 x = Int64.of_int x |> packn 1
 end
 
 module BE : Endian = struct
     let packn n x = pack_nbytes x n [] |> List.rev |> list_to_str
 
     let p64 = packn 8
-    let p32 = packn 4
-    let p16 = packn 2
-    let p8 = packn 1
+    let p32 x = Int64.of_int32 x |> packn 4
+    let p16 x = Int64.of_int x |> packn 2
+    let p8 x = Int64.of_int x |> packn 1
 end
--- lib/packing.mli
+++ lib/packing.mli
@@ -1,10 +1,8 @@
 module type Endian = sig
     val p64 : int64 -> string 
-    (*
-    val p32 : int32 -> char list
-    val p16 : int -> char list
-    val p8 : int -> char list
-    *)
+    val p32 : int32 -> string
+    val p16 : int -> string 
+    val p8 : int -> string 
 end
 
 module LE : Endian
