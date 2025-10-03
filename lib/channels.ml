1fd93656010a74afee67e98fff4b97ebb250b28d
nullenvk <nullenvk@protonmail.com>
2025-10-03 14:15:41 +0200

    Initial code for the Process_Channel module

--- lib/channels.ml
+++ lib/channels.ml
@@ -1,6 +1,18 @@
 (** Bidirectional channel type for communication with the pwned process **)
-(*
-module type Pwn_channel = sig
+module type Pwn_Channel = sig
+    type handle
+    type channel_params
 
+    val open_chan : channel_params -> handle
 end
-*)
+
+module Process_Channel : Pwn_Channel = struct
+    type handle = in_channel * out_channel
+    type channel_params = {
+        process_path : string;
+        process_args : string array;
+    }
+
+    let open_chan {process_path; process_args} = 
+        Unix.open_process_args process_path process_args
+end
--- lib/channels.mli
+++ lib/channels.mli
@@ -1,6 +1,7 @@
 (** Bidirectional channel type for communication with the pwned process **)
-(*
-module type Pwn_channel = sig
+module type Pwn_Channel = sig
+    type handle
+    type channel_params
 
+    val open_chan : channel_params -> handle
 end
-*)
